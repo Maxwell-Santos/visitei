4aea0ba811d6ce82e2fa006b2e7446c8df9e6770
Stephanie Gomes <oquevocenao@gmail.com>
2023-12-23 18:22:20 -0300

    Registrando lugar

--- controller/registrar.php
+++ controller/registrar.php
@@ -1,6 +1,4 @@
 <?php
-// $filename = '../dados.json';
-
 function adicionarLugar($nomeLocal, $titulo, $descricao, $commentDele, $commentDela, $preco, $data, $caminhoImagem1, $caminhoImagem2, $caminhoImagem3,
 $nota1, $nota2, $nota3, $media) {
 $filename = '../model/dados.json';
@@ -12,60 +10,47 @@ $filename = '../model/dados.json';
         $listaLugares = array('places' => array());
     }
 
-    // Novo card a ser adicionado
-    $novoCard = array(
-        'id' => count($listaLugares['places']) + 1,
-        'title' => $titulo,
-        'description' => $descricao,
-        'price' => $preco,
-        'date' => $data,
-        'imgs' => array(
-            'img1' => $caminhoImagem1,
-            'img2' => $caminhoImagem2,
-            'img3' => $caminhoImagem3
-        ),
-        'himComment' => $commentDele,
-        'herComment' => $commentDela,
-        'stars' => array(
-            'environment' => $nota1,
-            'logistic' => $nota2,
-            'price' => $nota3,
-            'average' => $media
-        )
-    );
+    foreach ($listaLugares['places'] as &$lugar) {
+        if ($lugar['name'] === $nomeLocal) {
+            
+            $id = count($lugar['cards'])+1;
 
-    // Encontrar o local existente ou criar um novo
-    $localExistente = false;
-    foreach ($listaLugares['places'] as $local) 
-    {
-        if ($local['name'] === $nomeLocal) 
-        {
-            $local['cards'][] = $novoCard;
-            $localExistente = true;
-            // $listaLugares['places'][$local['name']][] = $novoCard;
+            foreach ($lugar['cards'] as $cardLugar)
+            {
+                if ($cardLugar['id'] >= $id)
+                {
+                    $id = $cardLugar['id']+1;
+                }
+            }
+            $novoCard = array(
+                'id' => $id,
+                'title' => $titulo,
+                'description' => $descricao,
+                'price' => $preco,
+                'date' => $data,
+                'imgs' => array(
+                    'img1' => $caminhoImagem1,
+                    'img2' => $caminhoImagem2,
+                    'img3' => $caminhoImagem3
+                ),
+                'himComment' => $commentDele,
+                'herComment' => $commentDela,
+                'stars' => array(
+                    'environment' => $nota1,
+                    'logistic' => $nota2,
+                    'price' => $nota3,
+                    'average' => $media
+                )
+            );
+            $lugar['cards'][] = $novoCard;
             break;
         }
-        // $listaLugares['places'][$local['name']][] = $novoCard;
     }
 
-    if (!$localExistente) 
-    {
-        $novoLocal = array(
-            'name' => $nomeLocal,
-            'cards' => array($novoCard)
-        );
-        $listaLugares['places'][] = $novoLocal;
-
-    }
-
-    // Converter o array atualizado para JSON
     $novoJSON = json_encode($listaLugares, JSON_PRETTY_PRINT);
-
-    // Salvar o JSON atualizado no arquivo
     file_put_contents($filename, $novoJSON);
 }
 
-// Exemplo de chamada da função para adicionar um restaurante
 adicionarLugar(
     $_GET['categoria'],
     $_GET['titulo'],
