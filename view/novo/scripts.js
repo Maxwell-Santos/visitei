6f5be24ef9b46851435145bca1b671a3ec72f4d2
Maxwell-Santos <max.coding011@gmail.com>
2024-01-06 16:47:57 -0300

    adição dos input de imgs

--- view/novo/scripts.js
+++ view/novo/scripts.js
@@ -1,45 +1,45 @@
-const estrelas = document.querySelectorAll(".estrelas input")
-const notas = new Map()
+const estrelas = document.querySelectorAll(".estrelas input");
+const notas = new Map();
 
 estrelas.forEach(estrela => {
-  estrela.addEventListener('change', () => {
-    const estrelaId = estrela.id
-    notas.set(estrela.name, estrela.value)
+  estrela.addEventListener("change", () => {
+    const estrelaId = estrela.id;
+    notas.set(estrela.name, estrela.value);
 
-    mostrarMedia(calcularMedia())
+    mostrarMedia(calcularMedia());
 
-    console.log(estrela)
-  })
+    console.log(estrela);
+  });
 });
 
 function calcularMedia() {
-  let media = 0
+  let media = 0;
 
   notas.forEach(item => {
-    media = media + Number(item)
-  })
-  media = media / notas.size
+    media = media + Number(item);
+  });
+  media = media / notas.size;
 
-  return Math.round(media)
+  return Math.round(media);
 }
 
-const estrelasMedia = document.querySelectorAll("#estrelasMedia input")
+const estrelasMedia = document.querySelectorAll("#estrelasMedia input");
 
 function mostrarMedia(media) {
   estrelasMedia.forEach(estrela => {
     if (estrela.value == media) {
-      estrela.checked = true
-    } else estrela.checked = false
-  })
+      estrela.checked = true;
+    } else estrela.checked = false;
+  });
 }
 
-document.querySelector("#telefone").addEventListener('input', formatar);
+document.querySelector("#telefone").addEventListener("input", formatar);
 
 function formatar(event) {
   let valor = event.target.value;
 
   // Remove todos os caracteres não numéricos
-  valor = valor.replace(/\D/g, '');
+  valor = valor.replace(/\D/g, "");
 
   // Adiciona a máscara de Reais enquanto digita
   if (valor.length > 2) {
@@ -52,8 +52,37 @@ function formatar(event) {
   event.target.value = valor;
 }
 
-const containerEstrelas = document.querySelectorAll('.sessao-notas div')
+const containerEstrelas = document.querySelectorAll(".sessao-notas div");
 
 function checarEstrelasSelecionadas() {
-  console.log(containerEstrelas)
+  console.log(containerEstrelas);
+}
+
+/**
+ * selecionar img do card
+ */
+const imgs = {
+  "inpt-img1": document.querySelector("#img1"),
+  "inpt-img2": document.querySelector("#img2"),
+  "inpt-img3": document.querySelector("#img3"),
+};
+function addImage(idInput) {
+  const input = document.getElementById(idInput);
+
+  input.click();
+
+  input.onchange = event => {
+    if (!(event.target && event.target.files && event.target.files.length > 0))
+      return;
+
+    var r = new FileReader();
+    r.onload = function () {
+      Object.entries(imgs).forEach(([prop, value]) => {
+        if (prop == idInput) {
+          value.src = r.result;
+        }
+      });
+    };
+    r.readAsDataURL(event.target.files[0]);
+  };
 }
